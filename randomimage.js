85aaf0461c819009dbc020cd8c1e420fc46c9335
あしらーと <154391891+Asiraat@users.noreply.github.com>
2023-12-30 10:25:14 +0900

    Update randomimage.js

--- randomimage.js
+++ randomimage.js
@@ -1,19 +1,10 @@
-// 仮の画像URLの配列（実際のランダムな画像URLを取得する方法はサーバーサイドで処理する必要があります）
-const imageUrls = [
-  "https://Asiraat.github.io/Prototype/picture2/00004-975062830.png",
-  "https://Asiraat.github.io/Prototype/picture2/00045-712403987.png",
-  "https://Asiraat.github.io/Prototype/picture2/00049-768448109.png"
-  "https://Asiraat.github.io/Prototype/picture2/00119-814775075.png"
-
-  // 他の画像URLを追加
-];
-
 // ページが読み込まれた時に実行される処理
 window.onload = function() {
-  const randomIndex = Math.floor(Math.random() * imageUrls.length);
-  const randomImageUrl = imageUrls[randomIndex];
+  const randomIndex = Math.floor(Math.random() * 12) + 1; // 1から12のランダムな数値を生成
+  const randomImageUrl = `https://Asiraat.github.io/Prototype/picture2/${randomIndex}.png`; // ランダムな数値を含む画像URLを生成
 
   // ランダムな画像を取得して表示
   const randomImageElement = document.getElementById("random-image");
   randomImageElement.src = randomImageUrl;
 };
+
