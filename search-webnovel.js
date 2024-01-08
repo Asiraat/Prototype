1fa29194345ba678d12170d24aacfd34b4b614de
あしらーと <154391891+Asiraat@users.noreply.github.com>
2024-01-08 14:09:47 +0900

    Update search-webnovel.js

--- search-webnovel.js
+++ search-webnovel.js
@@ -21,24 +21,13 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 /*
+クラス名がcollapse-btn の要素がクリックされた時に、クラス名がtag-list の要素を表示・非表示にする
+*/
 document.addEventListener("DOMContentLoaded", function() {
-  const tagButtons = document.querySelectorAll(".tag-btn");
-  const boxContainers = document.querySelectorAll(".box-container");
+  const collapseBtn = document.querySelector(".collapse-btn");
+  const tagList = document.querySelector(".tag-list");
 
-  tagButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const selectedTag = button.getAttribute("data-tag");
-
-      boxContainers.forEach(box => {
-        const boxTags = box.getAttribute("data-tags").split(" ");
-
-        if (boxTags.includes(selectedTag) || selectedTag === "all") {
-          box.style.display = "block";
-        } else {
-          box.style.display = "none";
-        }
-      });
-    });
+  collapseBtn.addEventListener("click", () => {
+    tagList.classList.toggle("show-tags");
   });
 });
-*/
