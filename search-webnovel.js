04f468f80ab8f211db3fa20b352df27301b9f52b
あしらーと <154391891+Asiraat@users.noreply.github.com>
2024-01-08 15:08:07 +0900

    Update search-webnovel.js

--- search-webnovel.js
+++ search-webnovel.js
@@ -1,4 +1,30 @@
 // search-webnovel.js
+
+document.addEventListener("DOMContentLoaded", function() {
+  const collapseBtn = document.querySelector(".collapse-btn");
+  const tagList = document.querySelector(".tag-list");
+  const boxContainers = document.querySelectorAll(".box-container");
+
+  collapseBtn.addEventListener("click", () => {
+    tagList.classList.toggle("show-tags");
+  });
+
+  collapseBtn.addEventListener("click", () => {
+    const tagsToShow = ['all', 'tag1', 'tag2', 'tag3'];
+
+    boxContainers.forEach(box => {
+      const boxTags = box.getAttribute("data-tags").split(" ");
+
+      if (tagsToShow.some(tag => boxTags.includes(tag))) {
+        box.style.display = "block";
+      } else {
+        box.style.display = "none";
+      }
+    });
+  });
+});
+
+/*
 document.addEventListener("DOMContentLoaded", function() {
   const tagButtons = document.querySelectorAll(".tag-btn");
   const boxContainers = document.querySelectorAll(".box-container");
@@ -20,9 +46,7 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
-/*
-クラス名がcollapse-btn の要素がクリックされた時に、クラス名がtag-list の要素を表示・非表示にする
-*/
+
 document.addEventListener("DOMContentLoaded", function() {
   const collapseBtn = document.querySelector(".collapse-btn");
   const tagList = document.querySelector(".tag-list");
@@ -31,3 +55,5 @@ document.addEventListener("DOMContentLoaded", function() {
     tagList.classList.toggle("show-tags");
   });
 });
+
+*/
